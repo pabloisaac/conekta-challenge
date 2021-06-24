8c71501e45e1ce99b374874b5304d92389b4ba7c
Pablo Isaac <pabloisaac@MacBook-Pro-de-Pablo.local>
2021-06-24 16:11:19 -0300

    Update conekta-api urlbase

--- src/services/ApiConekta.js
+++ src/services/ApiConekta.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const urlBase = "https://161.22.44.36:7777";
+const urlBase = "ec2-3-143-216-169.us-east-2.compute.amazonaws.com:7777";
 
 export const login = async (username, password) => {
     let url = `${urlBase}/login`
